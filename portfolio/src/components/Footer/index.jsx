a236f7830a8204df704dee8b30f4ced8aac97fd3
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-14 22:43:19 -0300

    Melhorias css

---
+++ portfolio/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css"
+
+function Footer(){
+    return (
+        <footer className={styles.footer}>
+            Feito com ❤️ por Gustavo Miguel &copy; 2024
+        </footer>
+    );
+}
+
+export default Footer;
