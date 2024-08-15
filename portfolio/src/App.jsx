0f02b49503a0913aa32a3d8af6b26148fe2a4484
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-14 22:51:53 -0300

    add conteúdo

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,29 @@
-import './App.css'
-import Footer from './components/Footer';
-import Header from './components/Header'
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
-      Conteúdo
+      <section className="container">
+        <div className='apresentacao'>
+          <p>
+            Olá, sou <br />
+            <span>Gustavo Miguel</span> <br />
+            Dev Full Stack
+          </p>
+          <button className='btn btn-red'>
+            Saiba mais sobre mim
+          </button>
+        </div>
+        <figure>
+          <img className='img-home' src="/developer-red.svg" alt="Imagem de Home"/>
+        </figure>
+      </section>
       <Footer />
     </>
-  )
+  );
 }
 
 export default App;
