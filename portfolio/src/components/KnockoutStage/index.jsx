3c9488fa72ed79d50e5e6681ff5cf8a09571b66f
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-15 23:08:23 -0300

    css finalizado do componente KnockoutStage

---
+++ portfolio/src/components/KnockoutStage/index.jsx
@@ -0,0 +1,11 @@
+import styles from '.KnockoutStage.module.css'
+
+function KnockoutStage(){
+    return(
+        <section className={styles.jogos}>
+
+        </section>
+    );
+}
+
+export defualt KnockoutStage;
