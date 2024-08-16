6c7310ce6a54b61caf515b657748f688712501cc
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-15 23:20:10 -0300

    Nova lib instalada

--- portfolio/src/components/KnockoutStage/index.jsx
+++ portfolio/src/components/KnockoutStage/index.jsx
@@ -1,24 +1,29 @@
-import styles from '.KnockoutStage.module.css'
-import { useEffect, useState } from 'react';
+import styles from ".KnockoutStage.module.css";
+import { useEffect, useState } from "react";
 
-function KnockoutStage(){
+function KnockoutStage() {
+  const [jogos, setJogos] = useState("");
+  const url = `https://raw.githubusercontent.com/edsonmaia/apifakecopa2023/main/oitavas-copa-2023.json`;
 
-    const [ jogos, setJogos ] = useState('')
-    const url = `https://raw.githubusercontent.com/edsonmaia/apifakecopa2023/main/oitavas-copa-2023.json`
+  useEffect(() => {
+    const buscarJogos = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setJogos(data);
+    };
+  }, [url]);
 
-    useEffect( () => {
-        const buscarJogos = async () => {
-            const response = await fetch(url)
-            const data = await response.json()
-            setJogos(data)
-        }
-    }, [])
-
-    return(
-        <section className={styles.jogos}>
-
-        </section>
-    );
+  return (
+    <section className={styles.jogos}>
+      {jogos.map((jogo) => (
+        <div key={jogo.jogo}>
+          <h2>
+            Oitavas {jogo.jogo} - chave {jogo.chave}
+          </h2>
+        </div>
+      ))}
+    </section>
+  );
 }
 
 export default KnockoutStage;
--- portfolio/src/pages/Contatos/index.jsx
+++ portfolio/src/pages/Contatos/index.jsx
@@ -1,13 +1,19 @@
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import styles from "./Contatos.module.css";
 
 function Contatos() {
   return (
     <>
       <Header />
       <Container>
-        <h2>Sobre</h2>
+        <section className={styles.contatos}>
+          <h2>Contatos</h2>
+          <h3>Entre em contato</h3>
+          <p>Para que possamos conversar mais sobre.</p>
+          <div className={styles.icones}>ícones</div>
+        </section>
       </Container>
       <Footer />
     </>
