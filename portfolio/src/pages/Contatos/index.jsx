d52ced9d786dc277f42fb555bc974772178dfbf2
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-15 23:34:51 -0300

    add link externo para os ico

--- portfolio/src/pages/Contatos/index.jsx
+++ portfolio/src/pages/Contatos/index.jsx
@@ -18,12 +18,23 @@ function Contatos() {
           <h2>Contatos</h2>
           <h3>Entre em contato</h3>
           <p>Para que possamos conversar mais sobre.</p>
-          <div className={styles.icones}>ícones
-            <GoMail />
-            <BsInstagram />
-            <BsYoutube />
-            <BsGithub />
-            <BsLinkedin />
+          <div className={styles.icones}>
+
+            <a href='mailto:gustavomiguelsilvafc@gmail.com' target='_blank' rel='noopener noreferrer'>
+            <GoMail className={styles.icone}/></a>
+
+            <a href='https://www.instagram.com/gxstavo_miguel' target='_blank' rel='noopener noreferrer'>
+            <BsInstagram className={styles.icone}/></a>
+
+            <a href='https://www.youtube.com/@zgxstavo' target='_blank' rel='noopener noreferrer'>
+            <BsYoutube className={styles.icone}/>  </a>
+
+            <a href='https://github.com/gxstavomiguel' target='_blank' rel='noopener noreferrer'>
+            <BsGithub className={styles.icone}/> </a>
+
+
+            <BsLinkedin className={styles.icone}/>
+
           </div>
         </section>
       </Container>
