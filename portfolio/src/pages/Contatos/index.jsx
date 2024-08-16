1ab87f4ba2ddaf0d02d144821c28d0e481054ae7
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-15 23:25:22 -0300

    add icon

--- portfolio/src/pages/Contatos/index.jsx
+++ portfolio/src/pages/Contatos/index.jsx
@@ -2,6 +2,12 @@ import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import styles from "./Contatos.module.css";
+import { GoMail } from 'react-icons/go'
+import { BsInstagram, BsYoutube, BsGithub, BsLinkedin } from 'react-icons/bs'
+
+
+
+
 
 function Contatos() {
   return (
@@ -12,7 +18,13 @@ function Contatos() {
           <h2>Contatos</h2>
           <h3>Entre em contato</h3>
           <p>Para que possamos conversar mais sobre.</p>
-          <div className={styles.icones}>ícones</div>
+          <div className={styles.icones}>ícones
+            <GoMail />
+            <BsInstagram />
+            <BsYoutube />
+            <BsGithub />
+            <BsLinkedin />
+          </div>
         </section>
       </Container>
       <Footer />
