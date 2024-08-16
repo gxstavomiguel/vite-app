a50bbc63635b5ad57a342372fb1d032e02d5afa2
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-15 23:13:06 -0300

    Consulta a API fake para acessar os dados dos jogos

--- portfolio/src/components/KnockoutStage/index.jsx
+++ portfolio/src/components/KnockoutStage/index.jsx
@@ -1,6 +1,19 @@
 import styles from '.KnockoutStage.module.css'
+import { useEffect, useState } from 'react';
 
 function KnockoutStage(){
+
+    const [ jogos, setJogos ] = useState('')
+    const url = `https://raw.githubusercontent.com/edsonmaia/apifakecopa2023/main/oitavas-copa-2023.json`
+
+    useEffect( () => {
+        const buscarJogos = async () => {
+            const response = await fetch(url)
+            const data = await response.json()
+            setJogos(data)
+        }
+    }, [])
+
     return(
         <section className={styles.jogos}>
 
@@ -8,4 +21,4 @@ function KnockoutStage(){
     );
 }
 
-export defualt KnockoutStage;
+export default KnockoutStage;
