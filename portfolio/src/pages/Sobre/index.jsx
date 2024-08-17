6c323f47d97158e5fb734949ae9b49daea75b8ce
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-16 22:52:03 -0300

    add conteúdo

--- portfolio/src/pages/Sobre/index.jsx
+++ portfolio/src/pages/Sobre/index.jsx
@@ -1,8 +1,29 @@
+import styles from "./Sobre.module.css";
+import avatar from "./images/avatar.png";
+
 function Sobre() {
   return (
-    <>
-      <h2>Sobre</h2>
-    </>
+    <section className={styles.sobre}>
+
+      <div className={styles.bio}>
+        <img src={avatar} alt="imagem de perfil" className={styles.avatar} />
+        <div>
+          <h2>Sobre</h2>
+
+          <p>Sou Edson Maia <br />
+            Dev Full Stack</p>
+          <p>Trabalho com desenvolvimento Web desde 2000.</p>
+
+          <p>Sou apaixonado por transformar ideias em realidade digital.</p>
+
+          <p>Especializado em criação de aplicações dinâmicas e intuitivas, <br /> com foco na experiência do usuário.</p>
+        </div>
+      </div>
+      <div className={styles.tech}>
+        <h3>Techs</h3>
+        images
+      </div>
+    </section>
   );
 }
 
