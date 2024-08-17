6e91c5dba38ff2678fc5eadec1dbe346ddd9de99
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-16 23:11:51 -0300

    novo componente criado

---
+++ portfolio/src/components/Card/index.jsx
@@ -0,0 +1,16 @@
+import styles from ".Card.module.css";
+
+function Card() {
+  return (
+    <section className={styles.card}>
+      <h3>Título do projeto</h3>
+      <p>Texto descritivo do projeto</p>
+      <div className={styles.card_footer}>
+        <div className={styles.card_icones}>html</div>
+        <button className={styles.botao}>Seta</button>
+      </div>
+    </section>
+  );
+}
+
+export default Card;
