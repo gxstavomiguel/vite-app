fda43b4d659b439aa1502a6297ddf6bf4cfa7e45
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-16 23:31:50 -0300

    +css

--- portfolio/src/pages/Projetos/index.jsx
+++ portfolio/src/pages/Projetos/index.jsx
@@ -1,13 +1,18 @@
 import Card from "../../components/Card";
+import styles from "./Projeto.module.css";
 
 function Projetos() {
   return (
-    <>
+    <section className={styles.projeto}>
       <h2>Sobre</h2>
-      <Card />
-      <Card />
-      <Card />
-    </>
+      <section className={styles.lista}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </section>
+    </section>
   );
 }
 
