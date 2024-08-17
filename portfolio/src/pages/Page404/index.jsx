dd6ab9981a74767c832a22f755513eb44f604872
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-17 16:13:49 -0300

    responsi page404

--- portfolio/src/pages/Page404/index.jsx
+++ portfolio/src/pages/Page404/index.jsx
@@ -2,7 +2,7 @@ import styles from "./Page404.module.css";
 
 function Page404() {
   return (
-    <>
+    <section className={styles.page404}>
 
         <h2 className={styles.titulo2}>Algo de errado não está certo!</h2>
         <div className={styles.textos}>
@@ -10,7 +10,7 @@ function Page404() {
           <strong className={styles.texto_vermelho}>Página não localizada!</strong>
         </div>
     
-    </>
+    </section>
   );
 }
 
