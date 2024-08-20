ca5b3fbf2768c57e24a869e582800fa4f4084ed0
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-19 23:55:25 -0300

    toggleMenu add

--- portfolio/src/components/Header/index.jsx
+++ portfolio/src/components/Header/index.jsx
@@ -1,29 +1,31 @@
 import { Link } from "react-router-dom";
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
+import { useState } from "react";
 
-function Header(){
-    return (
-        <header className={styles.header}>
-            <Link to="/">
-            
-            <span>
-                GustavoMiguel.dev
-            </span>
-            </Link>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/sobre">Sobre</Link>
-                <Link to="/projetos">Projetos</Link>
-                <Link to="/contatos">Contatos</Link>
+function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
-            </nav>
-            <div className={styles.menuButton}>
-                <span className={styles.linha}></span>
-                <span className={styles.linha}></span>
-                <span className={styles.linha}></span>
-            </div>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <Link to="/">
+        <span>GustavoMiguel.dev</span>
+      </Link>
+      <nav className={styles.menuSandwich}>
+        <Link to="/">Home</Link>
+        <Link to="/sobre">Sobre</Link>
+        <Link to="/projetos">Projetos</Link>
+        <Link to="/contatos">Contatos</Link>
+      </nav>
+      <div onClick={toggleMenu} className={styles.menuButton}>
+        <span className={styles.linha}></span>
+        <span className={styles.linha}></span>
+        <span className={styles.linha}></span>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
